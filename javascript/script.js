f2f6eb2c8aada54a79fce1f474f944c3e3f3728c
Steven Phun <phun.steven@gmail.com>
2022-02-16 10:26:54 -0800

    start .env

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,15 @@
+import 'dotenv/config' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from 'express'
+
+require('dotenv').config()
+console.log(process.env)
+
 function fetchNews() {
+    require('dotenv').config()
+    const result = dotenv.config()
+    console.log(result.parsed);
+    const apiKey = process.env.NEWS_API_KEY
+    // console.log(apiKey);
     const url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${apiKey}`;
     let request = new XMLHttpRequest()
     request.addEventListener("load", responseReceivedHandler)
@@ -10,16 +21,14 @@ function fetchNews() {
 function responseReceivedHandler() {
     console.log(this.status);
     if (this.status === 200) {
-        document.querySelector(".test").innerHTML = this.response
         displayNews(this.response["articles"])
-
     }
 }
 
 function displayNews(articles) {
-    for (let i = 0; i < articles; i++) {
-        console.log(articles[i]);
-        articles[i]["title"]
+    console.log(articles);
+    for (let i = 0; i < articles.length; i++) {
+        document.querySelector(".test").innerHTML += articles[i]["title"] + "<br>";
     }
 }
 
