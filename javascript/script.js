131e3aba166a72dfa84f05501cca25e3797e4464
Steven Phun <phun.steven@gmail.com>
2022-02-16 11:39:16 -0800

    code clean up

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 function fetchNews() {
-    const apiKey = ''
+    const apiKey = '6ceb3e705a004d81b3e940276efefd61'
     const url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${apiKey}`
     let request = new XMLHttpRequest()
     request.addEventListener("load", responseReceivedHandler)
@@ -11,15 +11,39 @@ function fetchNews() {
 function responseReceivedHandler() {
     console.log(this.status);
     if (this.status === 200) {
-        displayNews(this.response["articles"])
+        displayNews(this.response)
+    } else {
+        console.log(this.status);
     }
 }
 
-function displayNews(articles) {
-    console.log(articles);
-    for (let i = 0; i < articles.length; i++) {
-        document.querySelector(".test").innerHTML += articles[i]["title"] + "<br>";
+function displayNews(response) {
+    // check response
+    console.log(response);
+
+    // properties
+    const articles = response['articles']
+
+    // tags
+    const articleTag = document.querySelector("#news")
+
+    // keys
+    const author = 'author'
+    const content = 'content'
+    const description = 'description'
+    const publishedAt = 'publishedAt'
+    const source = 'source'
+    const title = 'title'
+    const url = 'url'
+    const imageUrl = 'urlToImage'
+
+    // parse news data
+    let newsContent
+    for (const article of articles) {
+        newsContent += `${article[title]} <br>`
     }
+
+    articleTag.innerHTML = newsContent
 }
 
 fetchNews()
